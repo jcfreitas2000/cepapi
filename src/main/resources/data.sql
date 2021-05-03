75334828b0d67d2fb715fb54d6167bc504023212
José Carlos de Freitas <jcfreitas2000@hotmail.com>
2021-05-02 21:49:45 -0300

    mocking cep with inserts in h2 database and replaced application properties by yaml

---
+++ src/main/resources/data.sql
@@ -0,0 +1,66 @@
+INSERT INTO cep(id, cep, street, neighborhood, city, state)
+VALUES
+(
+    'ddcd37bd-7891-4ba5-be8d-b26a67c1cd07',
+    '12345678',
+    'Street 12345678',
+    'Neighborhood 12345678',
+    'City 12345678',
+    'State 12345678'
+),
+(
+    'df2a86d6-2336-4f07-b39d-4b01f354cfcd',
+    '12345670',
+    'Street 12345670',
+    'Neighborhood 12345670',
+    'City 12345670',
+    'State 12345670'
+),
+(
+    '374983ca-fe2a-4c97-9887-7bcd1ffa7e5b',
+    '12345600',
+    'Street 12345600',
+    'Neighborhood 12345600',
+    'City 12345600',
+    'State 12345600'
+),
+(
+    'a96dc478-7fb1-4cc9-af98-8c0fc3fff985',
+    '12345000',
+    'Street 12345000',
+    'Neighborhood 12345000',
+    'City 12345000',
+    'State 12345000'
+),
+(
+    'bb568b8c-54a9-464a-b64a-035a4ea61611',
+    '12340000',
+    'Street 12340000',
+    'Neighborhood 12340000',
+    'City 12340000',
+    'State 12340000'
+),
+(
+    'a7972c4d-05c0-441d-81ab-41e592c13ba3',
+    '12300000',
+    'Street 12300000',
+    'Neighborhood 12300000',
+    'City 12300000',
+    'State 12300000'
+),
+(
+    '676895d5-f3ac-4924-9a90-a8a336de4acb',
+    '12000000',
+    'Street 12000000',
+    'Neighborhood 12000000',
+    'City 12000000',
+    'State 12000000'
+),
+(
+    'f73fecf3-a643-4e23-8d21-bd74e4fa9e97',
+    '10000000',
+    'Street 10000000',
+    'Neighborhood 10000000',
+    'City 10000000',
+    'State 10000000'
+);
