bcb2d33dd2164166c43ddcb9a8cbbb555988d429
José Carlos de Freitas <jcfreitas2000@hotmail.com>
2021-05-02 23:25:39 -0300

    creating findAllByCepInOrderByCepDesc method to CepRepository and unit tests

--- src/main/java/br/tec/josecarlos/cepapi/cep/CepRepository.java
+++ src/main/java/br/tec/josecarlos/cepapi/cep/CepRepository.java
@@ -2,7 +2,12 @@ package br.tec.josecarlos.cepapi.cep;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public interface CepRepository extends CrudRepository<Cep, UUID> {
+
+    List<Cep> findAllByCepInOrderByCepDesc(Collection<String> ceps);
+
 }
--- src/test/java/br/tec/josecarlos/cepapi/cep/CepRepositoryTest.java
+++ src/test/java/br/tec/josecarlos/cepapi/cep/CepRepositoryTest.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -35,5 +37,35 @@ class CepRepositoryTest {
     @Test
     public void shouldHaveInitialCepData_whenStartApplication() {
         assertEquals(8, cepRepository.count());
+    }
+
+    @Test
+    public void findAllByCepInOrderByCepDesc_shouldReturnAllCepsSortedDesc() {
+        List<Cep> cepList = cepRepository.findAllByCepInOrderByCepDesc(
+                Stream.of(
+                        "12345678",
+                        "12345670",
+                        "12345600",
+                        "12345000",
+                        "12340000",
+                        "12300000",
+                        "12000000",
+                        "10000000"
+                ).collect(Collectors.toSet())
+        );
+
+        assertEquals(8, cepList.size());
+        assertTrue(containsCep(cepList, "12345678"));
+        assertTrue(containsCep(cepList, "12345670"));
+        assertTrue(containsCep(cepList, "12345600"));
+        assertTrue(containsCep(cepList, "12345000"));
+        assertTrue(containsCep(cepList, "12340000"));
+        assertTrue(containsCep(cepList, "12300000"));
+        assertTrue(containsCep(cepList, "12000000"));
+        assertTrue(containsCep(cepList, "10000000"));
+    }
+
+    private boolean containsCep(List<Cep> cepList, final String cepNumber) {
+        return cepList.stream().filter(cep -> cep.getCep().equals(cepNumber)).count() == 1;
     }
 }
